eefbd97484ee33285751b6aac91a27eb5ff3fc5e
anderson-ru <iur.ru.miller@gmail.com>
2022-12-15 10:33:32 +0300

    Initial commit

---
+++ src/main/java/ru/vkbot/Bot.java
@@ -0,0 +1,63 @@
+package ru.vkbot;
+
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.GroupActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.messages.*;
+import com.vk.api.sdk.queries.messages.MessagesGetLongPollHistoryQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Bot {
+    public static void main(String[] args) throws ClientException, ApiException, InterruptedException
+    {
+        TransportClient transportClient = new HttpTransportClient();
+        VkApiClient vk = new VkApiClient(transportClient);
+        Random random = new Random();
+        Keyboard keyboard = new Keyboard();
+        //int id = 217762775;
+        //String token = "vk1.a.JVCk6wfsRPNF2mdilVhn6k6fN7Lf1qJM7-oAbyNA-BdlUzDtspVEW-UsBD7z0441QgjrQOp_Sv8UrcBq8PsTcgBbrqT5bZgChKOskNgAlmufc38b5KdnvfKO0eX_lVxU5QBqibT0GGEaAJ-e8LNBqO0MLzP0-Np4u7RrQ5_ZYcqpW-RPsxYP6PsDyaFy9Mxh8GhMRrESB2KqLSmKuPZ8tg";
+
+        List<List<KeyboardButton>> allKey = new ArrayList<>();
+        List<KeyboardButton> line1 = new ArrayList<>();
+        line1.add(new KeyboardButton().setAction(new KeyboardButtonAction().setLabel("Привет")).setColor(KeyboardButtonColor.POSITIVE));
+        line1.add(new KeyboardButton().setAction(new KeyboardButtonAction().setLabel("Кто я?")).setColor(KeyboardButtonColor.POSITIVE));
+        allKey.add(line1);
+        keyboard.setButtons(allKey);
+        //GroupActor actor = new GroupActor(groupid: 217762775, accessToken: "vk1.a.JVCk6wfsRPNF2mdilVhn6k6fN7Lf1qJM7-oAbyNA-BdlUzDtspVEW-UsBD7z0441QgjrQOp_Sv8UrcBq8PsTcgBbrqT5bZgChKOskNgAlmufc38b5KdnvfKO0eX_lVxU5QBqibT0GGEaAJ-e8LNBqO0MLzP0-Np4u7RrQ5_ZYcqpW-RPsxYP6PsDyaFy9Mxh8GhMRrESB2KqLSmKuPZ8tg");
+        GroupActor actor = new GroupActor(217762775, "vk1.a.JVCk6wfsRPNF2mdilVhn6k6fN7Lf1qJM7-oAbyNA-BdlUzDtspVEW-UsBD7z0441QgjrQOp_Sv8UrcBq8PsTcgBbrqT5bZgChKOskNgAlmufc38b5KdnvfKO0eX_lVxU5QBqibT0GGEaAJ-e8LNBqO0MLzP0-Np4u7RrQ5_ZYcqpW-RPsxYP6PsDyaFy9Mxh8GhMRrESB2KqLSmKuPZ8tg");
+
+        Integer ts = vk.messages().getLongPollServer(actor).execute().getTs();
+        while (true){
+            MessagesGetLongPollHistoryQuery historyQuery =  vk.messages().getLongPollHistory(actor).ts(ts);
+            List<Message> messages = historyQuery.execute().getMessages().getItems();
+            if (!messages.isEmpty()){
+                messages.forEach(message -> {
+                    System.out.println(message.toString());
+                    try {
+                        if (message.getText().equals("Привет")){
+                            vk.messages().send(actor).message("Привет!").userId(message.getFromId()).randomId(random.nextInt(10000)).execute();
+                        }
+                        else if (message.getText().equals("Кто я?")) {
+                            vk.messages().send(actor).message("Ты хороший человек.").userId(message.getFromId()).randomId(random.nextInt(10000)).execute();
+                        }
+                        /*else if (message.getText().equals("Кнопки")) {
+                            vk.messages().send(actor).message("А вот и они").userId(message.getFromId()).randomId(random.nextInt(10000)).keyboard(keyboard).execute();
+                        }*/
+                        else {
+                            vk.messages().send(actor).message("Я тебя не понял.").userId(message.getFromId()).randomId(random.nextInt(10000)).execute();
+                        }
+                    }
+                    catch (ApiException | ClientException e) {e.printStackTrace();}
+                });
+            }
+            ts = vk.messages().getLongPollServer(actor).execute().getTs();
+            Thread.sleep(500);
+        }
+    }
+}
